88b0e12af8b06815e857948a60beeee6211fdef3
Meryem Nobatova <meryem.nobatova.1@iliauni.edu.ge>
2025-04-24 11:11:34 +0400

    Delete t1 directory

--- t1/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"ListManupulation","l":"ListManupulation()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ListManupulation","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
